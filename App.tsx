881350e28913bc82d2fbc5f3f3af9f675a3e8cbf
nhatminh8702 <nhatminh8702@gmail.com>
2023-03-01 16:10:52 +0700

    first commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import SearchBar from './components/SearchBar';
+const App = () => {
+  return (
+    <View>
+      <SearchBar />
+    </View>
+  );
+};
+export default App;
